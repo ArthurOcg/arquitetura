eaa0c38b09e7cf510d985cdfe42bb4916555311f
arthurOcg <arthur.lima.oliveira@ccc.ufcg.edu.br>
2019-03-25 11:17:02 -0300

    Services implementado, eureka adicionado

--- Order/src/main/java/com/br/unifacisa/arquitetura/service/OrderService.java
+++ Order/src/main/java/com/br/unifacisa/arquitetura/service/OrderService.java
@@ -1,7 +1,11 @@
 package com.br.unifacisa.arquitetura.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.br.unifacisa.arquitetura.entidade.Order;
 import com.br.unifacisa.arquitetura.repository.OrderRepository;
 
 public class OrderService {
@@ -9,4 +13,24 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	public Order cadastrar(Order order)throws Exception {
+		try {
+			return orderRepository.save(order);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	public Optional<Order> consultaPorId(Long id) {
+		return orderRepository.findById(id); 	
+	}
+	
+	public List<Order> consultar(){
+		return (List<Order>) orderRepository.findAll();
+	}
+	
+	public void delet(Order order) {
+		orderRepository.delete(order);
+	}
+	
 }
--- Product/src/main/java/com/br/unifacisa/arquitetura/servico/ProductService.java
+++ Product/src/main/java/com/br/unifacisa/arquitetura/servico/ProductService.java
@@ -1,11 +1,33 @@
 package com.br.unifacisa.arquitetura.servico;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.br.unifacisa.arquitetura.entidade.Product;
 import com.br.unifacisa.arquitetura.repository.ProductRepository;
 
 public class ProductService {
 	
 	private ProductRepository productRepository;
 	
+	public Product cadastrar(Product product)throws Exception {
+		try {
+			return productRepository.save(product);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
 	
+	public Optional<Product> consultaPorId(Long id) {
+		return productRepository.findById(id); 	
+	}
+	
+	public List<Product> consultar(){
+		return (List<Product>) productRepository.findAll();
+	}
+	
+	public void delet(Product product) {
+		productRepository.delete(product);
+	}
 
 }
